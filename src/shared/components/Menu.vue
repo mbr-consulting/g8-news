51598115734e0d20724dc37e5fbdccbc8a988784
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-08 12:33:52 -0300

    add blog link

--- src/shared/components/Menu.vue
+++ src/shared/components/Menu.vue
@@ -7,20 +7,13 @@ import { MenuItem } from '.'
     <MenuItem label="PAGINA INICIAL" href="/" />
     <MenuItem label="CIDADES" href="/cidades" />
     <MenuItem label="G8 CONSTRUÇÕES" href="/g8-construcoes" />
+    <MenuItem label="BLOG" href="/blog" />
     <MenuItem label="FALE CONOSCO" href="/fale-conosco" />
     <!-- <a
       class="text-primary border-b-2 border-secondary font-bold hover:text-primary transition-colors pb-1"
       href="#"
       >PAGINA INICIAL</a
     >
-    <a class="text-on-surface-variant hover:text-primary transition-colors pb-1" href="#"
-      >CIDADES</a
-    >
-    <a class="text-on-surface-variant hover:text-primary transition-colors pb-1" href="#"
-      >G8 CONSTRUÇÕES</a
-    >
-    <a class="text-on-surface-variant hover:text-primary transition-colors pb-1" href="#"
-      >FALE CONOSCO</a
-    > -->
+    -->
   </nav>
 </template>
