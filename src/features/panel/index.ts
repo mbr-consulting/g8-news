81b64ddb22b1c62886a2702640c0d235f4c14f4c
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-09 09:20:41 -0300

    add panel pages

--- src/features/panel/index.ts
+++ src/features/panel/index.ts
@@ -1,3 +1,7 @@
-import DashboardPage from './modules/dashboard/pages/DashboardPage.vue'
+import PanelDashboardPage from './modules/dashboard/pages/DashboardPage.vue'
+import PanelPostsPage from './modules/posts/pages/PostPage.vue'
+import PanelAdsPage from './modules/ads/pages/AdsPage.vue'
+import PanelCitiesPage from './modules/cities/pages/CitiesPage.vue'
+import PanelUsersPage from './modules/users/pages/UsersPage.vue'
 
-export { DashboardPage }
+export { PanelDashboardPage, PanelPostsPage, PanelAdsPage, PanelCitiesPage, PanelUsersPage }
---
+++ src/features/panel/modules/cities/pages/CitiesPage.vue
@@ -0,0 +1,1 @@
+<template>Cities</template>
