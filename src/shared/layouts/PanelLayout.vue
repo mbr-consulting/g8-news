d1752a32623b6b43af00cd16346d5b4d0496e398
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-09 14:30:15 -0300

    remove comments

--- src/shared/layouts/PanelLayout.vue
+++ src/shared/layouts/PanelLayout.vue
@@ -6,14 +6,10 @@ import Menu from '@/features/panel/components/Menu.vue'
 <template>
   <div class="panel-theme">
     <div class="bg-background text-on-background font-body-md h-screen overflow-hidden flex">
-      <!-- SideNavBar -->
       <Menu />
-      <!-- Main Content Area -->
       <div class="flex-1 ml-64 flex flex-col h-screen overflow-hidden">
-        <!-- TopAppBar -->
         <Header />
-        <!-- Dashboard Content -->
-        <main class="flex-1 overflow-y-auto p-section-padding bg-surface-bright">
+        <main class="flex-1 overflow-y-auto p-8 bg-surface-bright">
           <RouterView />
         </main>
       </div>
