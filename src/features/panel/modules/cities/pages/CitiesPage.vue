b1978fe3d34957f65faa9851ab1200b9c1b3350c
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-10 15:01:59 -0300

    add page title in cities and users page

--- src/features/panel/modules/cities/pages/CitiesPage.vue
+++ src/features/panel/modules/cities/pages/CitiesPage.vue
@@ -1,1 +1,15 @@
-<template>Cities</template>
+<script setup lang="ts">
+import { PageTitle } from '@/features/panel/components'
+</script>
+
+<template>
+  <div class="max-w-container-max mx-auto flex flex-col gap-stack-lg">
+    <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-stack-md">
+      <PageTitle
+        title="Listagem de Cidades"
+        subtitle="Gerencie as cidades do portal."
+        :button="{ icon: 'add', label: 'Nova Cidade' }"
+      />
+    </div>
+  </div>
+</template>
--- src/features/panel/modules/users/pages/UsersPage.vue
+++ src/features/panel/modules/users/pages/UsersPage.vue
@@ -1,1 +1,15 @@
-<template>Users</template>
+<script setup lang="ts">
+import { PageTitle } from '@/features/panel/components'
+</script>
+
+<template>
+  <div class="max-w-container-max mx-auto flex flex-col gap-stack-lg">
+    <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-stack-md">
+      <PageTitle
+        title="Listagem de Usuários"
+        subtitle="Gerencie os usuários do portal."
+        :button="{ icon: 'add', label: 'Novo Usuário' }"
+      />
+    </div>
+  </div>
+</template>
