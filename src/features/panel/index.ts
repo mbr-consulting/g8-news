1aca70020b649c9bc1126ba687f061cfecaa3037
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-14 09:52:04 -0300

    add create user form initial screen

--- src/features/panel/index.ts
+++ src/features/panel/index.ts
@@ -3,5 +3,13 @@ import PanelPostsPage from './modules/posts/pages/PostPage.vue'
 import PanelAdsPage from './modules/ads/pages/AdsPage.vue'
 import PanelCitiesPage from './modules/cities/pages/CitiesPage.vue'
 import PanelUsersPage from './modules/users/pages/UsersPage.vue'
+import PanelUsersForm from './modules/users/pages/UsersForm.vue'
 
-export { PanelDashboardPage, PanelPostsPage, PanelAdsPage, PanelCitiesPage, PanelUsersPage }
+export {
+  PanelDashboardPage,
+  PanelPostsPage,
+  PanelAdsPage,
+  PanelCitiesPage,
+  PanelUsersPage,
+  PanelUsersForm,
+}
