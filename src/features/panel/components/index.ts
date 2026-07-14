21dfad3664b6d5002fe2350bb9f75a7e1ccf7f59
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-14 18:03:55 -0300

    add new components

--- src/features/panel/components/index.ts
+++ src/features/panel/components/index.ts
@@ -1,11 +1,26 @@
 import Badge from './Badge.vue'
 import Breadcrumb from './Breadcrumb.vue'
 import Button from './Button.vue'
+import Card from './Card.vue'
 import Header from './Header.vue'
 import Menu from './Menu.vue'
 import MenuItem from './MenuItem.vue'
 import PageTitle from './PageTitle.vue'
 import Table from './Table/Table.vue'
-import InputText from './input/InputText.vue'
 
-export { Menu, Badge, Button, Header, MenuItem, PageTitle, Table, InputText, Breadcrumb }
+import InputText from './input/InputText.vue'
+import InputCheckbox from './input/InputCheckbox.vue'
+
+export {
+  Menu,
+  Badge,
+  Button,
+  Card,
+  Header,
+  MenuItem,
+  PageTitle,
+  Table,
+  InputText,
+  InputCheckbox,
+  Breadcrumb,
+}
