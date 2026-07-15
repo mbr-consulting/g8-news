987f33d48de6cec3011d928f6430674431810192
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-15 10:25:43 -0300

    change style component

--- src/features/panel/components/PageTitle.vue
+++ src/features/panel/components/PageTitle.vue
@@ -5,11 +5,15 @@ import LinkTo from './LinkTo.vue'
 defineProps<PageTitleProps>()
 </script>
 <template>
-  <div>
-    <h1 class="font-headline-lg text-headline-lg text-primary">{{ title }}</h1>
-    <p class="font-body-sm text-body-sm text-on-surface-variant mt-1" v-if="subtitle">
-      {{ subtitle }}
-    </p>
+  <div class="max-w-container-max mx-auto flex flex-col gap-stack-lg">
+    <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-stack-md">
+      <div>
+        <h1 class="font-headline-lg text-headline-lg text-primary">{{ title }}</h1>
+        <p class="font-body-sm text-body-sm text-on-surface-variant mt-1" v-if="subtitle">
+          {{ subtitle }}
+        </p>
+      </div>
+      <LinkTo v-if="button" :icon="button.icon" :label="button.label" :route="button.route" />
+    </div>
   </div>
-  <LinkTo v-if="button" :icon="button.icon" :label="button.label" :route="button.route" />
 </template>
