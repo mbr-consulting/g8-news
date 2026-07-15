57d5e4a79c19654c1f669fde24ff581567e4c2ce
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-15 13:11:02 -0300

    add component columns

---
+++ src/features/panel/modules/posts/components/LeftColumn.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Button, Card, InputText, InputTextarea } from '@/features/panel/components'
+</script>
+
+<template>
+  <div class="md:col-span-8 flex flex-col gap-stack-lg">
+    <Card>
+      <div class="flex flex-col gap-stack-md">
+        <InputText
+          type="text"
+          label="Título da Publicação"
+          name="title"
+          placeholder="Insira um titulo chamativo..."
+        />
+        <InputText
+          type="text"
+          label="Subtítulo (Opcional)"
+          name="subtitle"
+          placeholder="Complemente a informação do título..."
+        />
+        <InputTextarea
+          label="Texto"
+          name="text"
+          placeholder="Comece a escrever sua matéria aqui..."
+          :rows="12"
+        />
+      </div>
+      <div class="flex justify-end gap-4 mt-6">
+        <Button variant="primary" type="submit" label="Publicar Agora" />
+        <Button variant="outline" type="button" label="Salvar Rascunho" />
+      </div>
+    </Card>
+  </div>
+</template>
---
+++ src/features/panel/modules/posts/components/RightColumn.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Card, InputDate, InputFile, InputSelect, InputTag } from '@/features/panel/components'
+import { Title } from '.'
+</script>
+
+<template>
+  <div class="md:col-span-4 flex flex-col gap-stack-lg">
+    <Card>
+      <Title title="Configurações de Publicação" />
+      <div class="flex flex-col gap-stack-md">
+        <InputSelect
+          label="Tipo de Postagem"
+          name="tipo-postagem"
+          :options="[
+            { value: 'publicacao', label: 'Publicação Padrão' },
+            { value: 'editorial', label: 'Editorial / Opinião' },
+          ]"
+        />
+        <InputDate label="Data de Publicação" name="data-publicacao" />
+        <InputTag />
+      </div>
+    </Card>
+    <Card>
+      <Title title="Imagem de Destaque" />
+      <InputFile
+        name="featured-image"
+        title="Clique para fazer upload"
+        subtitle="PNG, JPG ou WEBP (Max 5MB)"
+      />
+    </Card>
+  </div>
+</template>
