1ac0407e12c68b5448982fb9de87d0bce3ff0270
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-15 11:03:51 -0300

    add title component to posts

---
+++ src/features/panel/modules/posts/components/Title.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string
+}>()
+</script>
+<template>
+  <h3
+    class="font-headline-sm text-headline-sm text-on-surface border-b border-outline-variant pb-2"
+  >
+    {{ title }}
+  </h3>
+</template>
