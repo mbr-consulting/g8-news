d0fd012c6e11f9babd0a7534726ab0d3f10e75b1
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-15 17:44:39 -0300

    add icon to component

--- src/features/panel/components/Title.vue
+++ src/features/panel/components/Title.vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
 defineProps<{
   title: string
+  icon?: string
 }>()
 </script>
 <template>
   <h3
-    class="font-headline-sm text-headline-sm text-on-surface border-b border-outline-variant pb-2"
+    class="flex items-center gap-2 font-headline-sm text-headline-sm text-on-surface border-b border-outline-variant pb-2"
   >
+    <span v-if="icon" class="material-symbols-outlined text-primary fill" :data-icon="icon">
+      {{ icon }}
+    </span>
     {{ title }}
   </h3>
 </template>
