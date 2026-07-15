c9425de6fdffd6a48ca0633676c9cc7e5a130a59
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-15 13:12:32 -0300

    remove extra html tags

--- src/features/panel/modules/posts/pages/PostPage.vue
+++ src/features/panel/modules/posts/pages/PostPage.vue
@@ -3,13 +3,9 @@ import PageTitle from '@/features/panel/components/PageTitle.vue'
 </script>
 
 <template>
-  <div class="max-w-container-max mx-auto flex flex-col gap-stack-lg">
-    <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-stack-md">
-      <PageTitle
-        title="Listagem de Posts"
-        subtitle="Gerencie as publicações editoriais e notícias do portal."
-        :button="{ icon: 'add', label: 'Novo Post', route: { name: 'CreatePost' } }"
-      />
-    </div>
-  </div>
+  <PageTitle
+    title="Listagem de Posts"
+    subtitle="Gerencie as publicações editoriais e notícias do portal."
+    :button="{ icon: 'add', label: 'Novo Post', route: { name: 'CreatePost' } }"
+  />
 </template>
