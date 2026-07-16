0ec5c17e831d839cf8d032a66b587e2aedf8b089
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-16 18:03:49 -0300

    add post components

--- src/features/panel/modules/posts/components/index.ts
+++ src/features/panel/modules/posts/components/index.ts
@@ -1,4 +1,6 @@
 import LeftColumn from './LeftColumn.vue'
 import RightColumn from './RightColumn.vue'
+import PostStatus from './PostStatus.vue'
+import PostType from './PostType.vue'
 
-export { LeftColumn, RightColumn }
+export { LeftColumn, RightColumn, PostStatus, PostType }
