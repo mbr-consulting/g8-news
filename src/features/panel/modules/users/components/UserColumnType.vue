41b2d7d4e175f1cb6fdda4733233f7e1849e502a
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-17 13:55:51 -0300

    add user type badge component

---
+++ src/features/panel/modules/users/components/UserColumnType.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Badge } from '@/features/panel/components'
+defineProps<{
+  type: 'admin' | 'editor' | 'author'
+}>()
+</script>
+
+<template>
+  <Badge :label="type" />
+</template>
