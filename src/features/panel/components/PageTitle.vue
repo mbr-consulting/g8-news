f1b360cbf9f3fa7b7ff3ef1b1eadccd205ead4c3
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-07-20 13:45:34 -0300

    add component

---
+++ src/features/panel/components/PageTitle.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { PageTitleProps } from '@/shared/types'
+import LinkTo from './LinkTo.vue'
+
+defineProps<PageTitleProps>()
+</script>
+<template>
+  <div class="max-w-container-max mx-auto flex flex-col gap-stack-lg mb-5">
+    <div class="flex flex-col sm:flex-row sm:items-center justify-between gap-stack-md">
+      <div>
+        <h1 class="font-headline-lg text-headline-lg text-primary">{{ title }}</h1>
+        <p class="font-body-sm text-body-sm text-on-surface-variant mt-1" v-if="subtitle">
+          {{ subtitle }}
+        </p>
+      </div>
+      <LinkTo v-if="button" :icon="button.icon" :label="button.label" :route="button.route" />
+    </div>
+  </div>
+</template>
